286c8ef7a2ef8a4130036b89ade6c38a67961ba0
dumi994 <dumi.9944@gmail.com>
2021-05-05 23:13:05 +0200

    il programma genera il prezzo ma uguale per tutti

--- assets/js/main.js
+++ assets/js/main.js
@@ -72,15 +72,45 @@ function randomNumber (min, max){
 } */
 
 // chiiedere Nome passeggero
+var maggiorenne;
+var minorenne;
+var anziano;
+var prezzoBiglietto = 0.21;
+
+/* var scontoMaggiorenne = 0.21 ;
+var scontoMinorenne =  0.17;
+var scontoAnziano = 0.13;
+ */
 
 
-
+// questa funzione chiede il nome e lo stampa
 function myTicket(){
-    var userInputEl = document.getElementById('name').value;  
-    console.log(userInputEl);
-    document.getElementById('nome_passeggero').innerHTML = userInputEl;    
+    var userInputEl = document.getElementById('name').value;  //salva il nome in una var
+    console.log(userInputEl);       //log della var con il nome
+    document.getElementById('nome_passeggero').innerHTML = userInputEl;   //mette il valore della var nel id nome passeggero 
 }
-document.getElementById('generate_ticket_button').addEventListener('click', myTicket)
+document.getElementById('generate_ticket_button').addEventListener('click', myTicket) // mette in atto la funzione al click del bottone genera
+
+
+function distanceKm(){
+    var userDistanceEl = document.getElementById('km').value;
+    console.log(userDistanceEl);
+    document.getElementById('costo_totale').innerHTML = userDistanceEl;
+    var eta = document.getElementById('age').value;
+    console.log(eta);
+    if (eta.value === maggiorenne ){
+        document.getElementById('costo_totale').innerHTML = userDistanceEl * 0.17;
+    }else if(eta === minorenne){
+        document.getElementById('costo_totale').innerHTML = userDistanceEl * 0.21;
+
+    }else if (eta === anziano){
+        document.getElementById('costo_totale').innerHTML = userDistanceEl * 0.13;
+
+    }
+}
+document.getElementById('generate_ticket_button').addEventListener('click', distanceKm)
+
+
 
 // generare un Codice treno (numero casuale tra 90000 e 100000 escluso)
 /**
@@ -93,19 +123,14 @@ document.getElementById('generate_ticket_button').addEventListener('click', myTi
 function randomNumber (){
     var CP = Math.floor(Math.random() * (100000 - 90000) + 90000);
     var carrozze = Math.floor(Math.random() * (50 - 1))+ 1;
-    console.log(carrozze); 
+    console.log('la tua carrozza è la ' + carrozze); 
     document.getElementById('codice_cp').innerHTML = CP;
     document.getElementById('carrozza').innerHTML = carrozze;
 }
 
 document.getElementById('generate_ticket_button').addEventListener('click', randomNumber); 
 
-//document.getElementById('generate_ticket_button').addEventListener('click', carrozzaRandom);
 
-// assegnare Numero carrozza
+/* // assegnare Numero carrozza
 // Prezzo calcolato
-// Categoria selezionata dall'utente
-
-
-
-
+// Categoria selezionata dall'utente */
